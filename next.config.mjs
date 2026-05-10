cd9c41ba0a48d5b655151518388ada0ea6b954ee
Shaikh Mohd Kaif Mujamill <kaif282907@gmail.com>
2026-05-11 02:29:27 +0530

    Configure GitHub Pages deployment with GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo is NOT at kaif2910.github.io but at kaif2910.github.io/kaif-portfolio, 
+  // you might need to uncomment the lines below:
+  // basePath: '/kaif-portfolio',
+  // assetPrefix: '/kaif-portfolio',
+};
 
 export default nextConfig;
